e1b0751514c89ca3fa9ddcb4baef29e05f3eec0b
Muditha Sumanathunga <mudithadul@gmail.com>
2024-09-01 22:30:16 +0100

    fix: add unique columns

--- stripe-schema/src/main/resources/flyway.migrations.stripe/V1_1__initial_schema.sql
+++ stripe-schema/src/main/resources/flyway.migrations.stripe/V1_1__initial_schema.sql
@@ -3,7 +3,7 @@ create type credit_type as ENUM('SIGNUP', 'STRIPE');
 CREATE TABLE "user"
 (
     id SERIAL,
-    email      VARCHAR   NOT NULL,
+    email      VARCHAR   NOT NULL UNIQUE,
     CONSTRAINT user_id PRIMARY KEY (id)
 );
 
@@ -11,10 +11,10 @@ CREATE TABLE stripe_user
 (
     id SERIAL,
     stripe_user_id VARCHAR UNIQUE NOT NULL,
-    user_id int4 NOT NULL,
-    email      VARCHAR   NOT NULL,
+    user_id int4 NOT NULL UNIQUE,
+    email      VARCHAR   NOT NULL UNIQUE,
     created          TIMESTAMP NULL     DEFAULT timezone('utc'::TEXT, NOW()),
-    deleted          TIMESTAMP NULL     DEFAULT timezone('utc'::TEXT, NOW()),
+    deleted          TIMESTAMP NULL     DEFAULT NULL,
     CONSTRAINT stripe_user_id PRIMARY KEY (id),
     CONSTRAINT user_stripe_user_fk FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
@@ -26,9 +26,9 @@ CREATE TABLE stripe_transaction
     id SERIAL,
     stripe_user_id VARCHAR NOT NULL,
     amount money NOT NULL,
-    transaction_id   VARCHAR   NOT NULL,
+    transaction_id   VARCHAR   NOT NULL UNIQUE,
     created timestamp NULL DEFAULT timezone('utc'::text, now()),
-    deleted timestamp NULL DEFAULT timezone('utc'::text, now()),
+    deleted timestamp NULL DEFAULT NULL,
     CONSTRAINT stripe_transaction_id PRIMARY KEY (id),
     CONSTRAINT stripe_transaction_stripe_user_fk FOREIGN KEY (stripe_user_id) REFERENCES stripe_user (stripe_user_id)
 );
@@ -39,7 +39,7 @@ CREATE TABLE credit_account
     id SERIAL,
     user_id int4 NOT NULL,
     created timestamp NULL DEFAULT timezone('utc'::text, now()),
-    deleted timestamp NULL DEFAULT timezone('utc'::text, now()),
+    deleted timestamp NULL DEFAULT NULL,
     type credit_type NOT NULL ,
     token_count INT4 NOT NULL,
     CONSTRAINT credit_account_id PRIMARY KEY (id),
@@ -53,7 +53,8 @@ CREATE TABLE credit_usage
     user_id int4 NOT NULL,
     action_type VARCHAR NOT NULL ,
     created timestamp NULL DEFAULT timezone('utc'::text, now()),
-    credit_used INT4 NOT NULL,
+    deleted timestamp NULL DEFAULT NULL,
+    token_count INT4 NOT NULL,
     CONSTRAINT credit_usage_id PRIMARY KEY (id),
     CONSTRAINT credit_usage_user_fk FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
